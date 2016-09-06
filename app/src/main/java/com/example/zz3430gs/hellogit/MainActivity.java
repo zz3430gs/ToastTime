d3412e3a79d279762dd461f91f4587b9c39399e2
zz3430gs <keigolis95@gmail.com>
2016-09-06 14:19:32 -0500

    Added code into main

--- app/src/main/java/com/example/zz3430gs/hellogit/MainActivity.java
+++ app/src/main/java/com/example/zz3430gs/hellogit/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.zz3430gs.hellogit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("HEllo", "This is a test");
+
+        Button timeButton = (Button) findViewById(R.id.Button);
+        timeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String currentTime = new Date().toString();
+                Toast.makeText(MainActivity.this, "The time is "+ currentTime, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
