93362e602914275d2de33a99aa6db6e1fff37397
zz3430gs <keigolis95@gmail.com>
2016-09-06 14:10:45 -0500

    Added string name on strings.xml

---
+++ app/src/main/java/com/example/zz3430gs/hellogit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zz3430gs.hellogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
